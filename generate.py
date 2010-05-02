44e38e51f4023197a24c231b856cf670fbeea4cf
Brandon Thomas Suit <brandon.suit@gmail.com>
2010-05-02 00:34:50 -0400

    Significant changes to script, chapter 8 done

--- generate.py
+++ generate.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2.6
 
 import re
+import sys
+import os
 import markdown.markdown2 as markdown
 from markdown.template import header, footer, secHeader, secFooter
 
@@ -15,41 +17,114 @@ def writefile(fname, contents):
 	f.write(contents)
 	f.close()
 
+def getFilenameMatch(fname):
+	"""Finds the closest match to the filename. No html files returned."""
+	files = os.listdir('.')
+	if not files:
+		return False
+
+	blacklist = ['.html', '.py', '~'] # Cannot be in the filenames!
+
+	for f in files:
+		skip = False
+		for b in blacklist:
+			if b in f:
+				skip = True
+				break
+		if skip:
+			continue
+		if f.find(fname) == 0:
+			return f
+	
+	return False
+		
+	
 def markupFirst(x):
-	# Center line
-	x = re.sub(r'center: (.*)$', r'<p class="cen">\1</p>', x) 
-	# Center all
-	x = re.sub(r'centerAll: (.*)', r'<p class="cen">\1</p>', x, re.DOTALL) 
+	"""Markup to add before markdown parsed."""
+	# Global changes
+	x = re.sub(r'\t(.*)', r'\1', x) # Remove leading tab
+	x = re.sub(r'\* (.*) -- ', r'* **\1** - ', x) # auto-bold 
+	x = re.sub(r'\* (.*): ', r'* *\1*: ', x) # auto-italics
+	x = re.sub(r' - ', r' &ndash; ', x) # endash
+
+	# Arrows
+	x = re.sub(r'-?->', r'&rarr;', x) # ->
+	x = re.sub(r'<--?', r'&larr;', x) # <-
+
+	# Tables
+	x = re.sub(r'<t>', r'<table><tr><td>', x) # endash
+	x = re.sub(r'<col>', r'</td><td>', x) # endash
+	x = re.sub(r'<row>', r'</td></tr><tr><td>', x) # endash
+	x = re.sub(r'</t>', r'</td></tr></table>', x) # endash
+	
+	# Global molecule/etc. notation
+	x = re.sub(r'[Cc]a\+2', r'Ca<sup>+2</sup>', x) # Calcium
+	x = re.sub(r'IPv3', r'IP<sub>3</sub>', x) # IP3
+	x = re.sub(r'alpha', r'&alpha;', x) # Alpha
+	x = re.sub(r'beta', r'&beta;', x) # Beta
+	x = re.sub(r'gamma', r'&gamma;', x) # Gamma
+	x = re.sub(r'a/b', r'&alpha;/&beta;', x) # Alpha/Beta
+
+	# Directives
+	x = re.sub(r'center: (.*)$', r'<p class="cen">\1</p>', x) # Center line
+	x = re.sub(r'centerAll: (.*)', r'<p class="cen">\1</p>', x, re.DOTALL) # Center all
+
+	print x
 	return x
 
 def markupAfter(x):
+	"""Markup to add after markdown parsed."""
 	# External links
-	x = re.sub(r'<a href="(.*)">(.*)</a>', 
-			   r'<a href="\1" rel="external">\2</a>', x)
-	x = re.sub(r'<ul>', 
-			   r'<ul class="incremental">', x) # show-first
+	#x = re.sub(r'<a href="(.*)">(.*)</a>', 
+	#		   r'<a href="\1" rel="external">\2</a>', x)
+	#x = re.sub(r'<ul>', 
+	#		   r'<ul class="incremental">', x) # show-first
+
+	# Remove paragraphs until I figure out markdown on paragraphs...
+	x = re.sub(r'<p>', r'', x)
+	x = re.sub(r'</p>', r'', x)
+
 	return x
 
-parsed = []
-for x in d.split("\n\n"):
-	if not x.strip():
-		continue
+def parseContents(data):
+	"""Run the content through the markdown parser and my own parsing"""
+	parsed = []
+	for x in data.split("\n\n"):
+		if not x.strip():
+			continue
 
-	# Comments
-	if x[0:4] == '<!--':
-		continue
-	if x[0:5] == '-----':
-		continue
+		# Comments
+		if x[0:4] == '<!--':
+			continue
+		if x[0:5] == '-----':
+			continue
 
-	x = markupFirst(x)
+		x = markupFirst(x)
 
-	parsed.append(markdown.markdown(x))
+		parsed.append(markdown.markdown(x))
 
-output = header + "\n"
-for x in parsed:
-	x = markupAfter(x)
-	output += slideHeader + x + slideFooter + "\n"
+	output = header + "\n"
+	for x in parsed:
+		x = markupAfter(x)
+		output += x + "\n" #secHeader + x + secFooter + "\n"
 
-output += footer
+	output += footer
+	return output
 
+
+def main():
+	if len(sys.argv) < 2:
+		sys.exit("Need argument")
+
+	infile = getFilenameMatch(sys.argv[1])
+
+	if not infile:
+		sys.exit("File doesn't exist.")
+
+	outfile = infile + ".html"
+
+	data = parseContents(readfile(infile))
+	writefile(outfile, data)
+
+if __name__ == '__main__': main()
 
